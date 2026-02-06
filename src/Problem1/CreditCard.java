b61798013873dfb59021dcf12907165484b206b3
Justin Seaward <jut_seaward@yahoo.com>
2026-02-06 18:48:21 -0330

    created Creditcard class. Added private veriables and start of CreditCard constructor

---
+++ src/Problem1/CreditCard.java
@@ -0,0 +1,11 @@
+package Problem1;
+
+public class CreditCard {
+    private Money balance;
+    private Money creditLimit;
+    private Person owner;
+
+    public CreditCard(Person newCardHolder, Money limit){
+
+    }
+}
