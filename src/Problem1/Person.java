a864a1cc96da2d2f54b32afcb5fc8f2ecef25362
Justin Seaward <jut_seaward@yahoo.com>
2026-02-07 11:40:28 -0330

    updated Address and Person files to have constructors, added CreditCardDemo file with veriables and printouts

--- src/Problem1/Person.java
+++ src/Problem1/Person.java
@@ -5,7 +5,13 @@ public class Person {
     private String firstName;
     private Address home;
 
+    public Person(String lastName, String firstName, Address home){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.home = home;
+    }
+
     public String toString(){
-        return(firstName + " " + lastName + " " + home);
+        return(firstName + " " + lastName + ", " + home);
     }
 }
