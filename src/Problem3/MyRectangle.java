3713f7a492d7bd02d004ed598320e24770c4e68d
Justin Seaward <jut_seaward@yahoo.com>
2026-02-08 13:06:36 -0330

    added MyRectangle constructors

--- src/Problem3/MyRectangle.java
+++ src/Problem3/MyRectangle.java
@@ -6,5 +6,13 @@ public class MyRectangle {
     private MyPoint topLeft;
     private MyPoint bottomRight;
 
+    public MyRectangle(int x1, int y1, int x2, int y2){
+        topLeft = new MyPoint(x1,y1);
+        bottomRight = new MyPoint(x2,y2);
+    }
 
+    public MyRectangle(MyPoint topLeft, MyPoint bottomRight){
+        this.topLeft = new MyPoint(topLeft);
+        this.bottomRight = new MyPoint(bottomRight);
+    }
 }
