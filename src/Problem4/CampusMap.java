5fce6736b8848198308a5af6a56c4ac6a2b78fd3
Justin Seaward <jut_seaward@yahoo.com>
2026-02-09 20:40:16 -0330

    added isWalkwayFromTo() method and added comments

--- src/Problem4/CampusMap.java
+++ src/Problem4/CampusMap.java
@@ -1,7 +1,6 @@
 package Problem4;
 
 // Imported classes
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import Problem3.MyRectangle;
 import Problem2.MyLine;
@@ -14,21 +13,21 @@ public class CampusMap {
     private ArrayList<MyLine> walkways;
     private ArrayList<MyCircle> fountains;
 
-    // Constructor
+    // Non parameterized Constructor
     public CampusMap(){
         buildings = new ArrayList<>();
         walkways = new ArrayList<>();
         fountains = new ArrayList<>();
     }
 
-    // Method to add buildings to buildings array
+    // Method to add buildings to array
     public void addBuilding(MyRectangle newBuilding){
        buildings.add(newBuilding);
     }
 
     // Method to add walkway between buildings fif they are in the buildings array
     public void addWalkWay(MyRectangle from, MyRectangle to){
-        // if statement to determine if the buildings are in the buildings array which calculation to add walkway
+        // if statement to determine if the buildings are in the array with an add walkway
         if(buildings.contains(from) && buildings.contains(to)){
             int fromRectangleCenterX = (from.getTopLeftX() + from.getBottomRightX()) / 2;
             int fromRectangleCenterY = (from.getTopLeftY() + from.getBottomRightY()) / 2;
@@ -44,10 +43,12 @@ public class CampusMap {
         }
     }
 
+    // Add fountains to array
     public void addFountain(MyCircle newFountain){
         fountains.add(newFountain);
     }
 
+    // Calculate total length of walkways on the map
     public double calculateTotalWalkwayLength(){
         double totalLength = 0.0;
 
@@ -57,6 +58,7 @@ public class CampusMap {
         return totalLength;
     }
 
+    // Calculate total area of the fountain(s)
     public double calculateTotalFountainArea(){
         double totalArea = 0.0;
 
@@ -66,5 +68,24 @@ public class CampusMap {
         return totalArea;
     }
 
+    // Method returns true if there is a walkway between two buildings or false if there isn't a walkway
+    public boolean isWalkwayFromTo(MyRectangle fromBuilding, MyRectangle toBuilding){
+        if(buildings.contains(fromBuilding) && buildings.contains(toBuilding)){
+            int fromRectangleCenterX = (fromBuilding.getTopLeftX() + fromBuilding.getBottomRightX()) / 2;
+            int fromRectangleCenterY = (fromBuilding.getTopLeftY() + fromBuilding.getBottomRightY()) / 2;
+            int toRectangleCenterX = (toBuilding.getTopLeftX() + toBuilding.getBottomRightX()) / 2;
+            int toRectangleCenterY = (toBuilding.getTopLeftY() + toBuilding.getBottomRightY()) / 2;
+            MyPoint fromPoint = new MyPoint(fromRectangleCenterX,fromRectangleCenterY);
+            MyPoint toPoint = new MyPoint(toRectangleCenterX,toRectangleCenterY);
 
+            for(int i = 0; i < walkways.size(); i++){
+                if(walkways.get(i).getBegin().equals(fromPoint) && walkways.get(i).getEnd().equals(toPoint)
+                        || walkways.get(i).getBegin().equals(toPoint) && walkways.get(i).getEnd().equals(fromPoint))
+                        {
+                            return true;
+                        }
+            }
+        }
+        return false;
+    }
 }
